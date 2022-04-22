8598a30bba226e983f016811aa0634b92b006aa6
unknown <sabilihaqrafli@gmail.com>
2022-04-22 21:25:58 +0700

    update

---
+++ js/anim.js
@@ -0,0 +1,8 @@
+anime({
+  targets: '.square',
+  translateX: anime.stagger(100),
+  dekay: anime.stagger(500,{from: 'last'}),
+  duration: 3000,
+  loop: false
+});
+                                                                                                                                                            
